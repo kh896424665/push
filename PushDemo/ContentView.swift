55b489b056fa0342ae33934354845a34acf9d050
“kanghui” <896424664@qq.com>
2024-09-08 23:51:40 +0800

    Initial Commit

---
+++ PushDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushDemo
+//
+//  Created by 康辉 on 2024/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushDemo/PushDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushDemoApp.swift
+//  PushDemo
+//
+//  Created by 康辉 on 2024/9/8.
+//
+
+import SwiftUI
+
+@main
+struct PushDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
